fbbca317e939f3adb87238e8fea9247480e0a2ba
Max Thrun <bear24rw@gmail.com>
2013-09-10 20:33:46 -0400

    boot straight to contra

--- software/install.sh
+++ software/install.sh
@@ -14,3 +14,29 @@ echo "Editing boot args to disable hdmi"
 echo "optargs=quiet drm.debug=7 capemgr.disable_partno=BB-BONELT-HDMI,BB-BONELT-HDMIN" > /mnt/card/uEnv.txt
 echo "Unmounting partition"
 umount /mnt/card/
+echo "Disabling some services"
+systemctl disable mpd
+systemctl disable gdm
+echo "Enabling autologin"
+cp getty@tty1.service /etc/systemd/system/getty.target.wants/
+echo "Changing default shell to bash"
+chsh -s /bin/bash
+echo "Intalling bash_profile"
+cp bash_profile .bash_profile
+echo "Disabling git sslVerify"
+git config --global http.sslVerify false
+echo "Updating opkg"
+opkg update
+echo "Installing python-distutils"
+opkg install python-distutils
+echo "Installing python-compile"
+opkg install python-compile
+
+# wget http://prdownloads.sourceforge.net/scons/scons-2.3.0.tar.gz
+# tar xvf scons-2.3.0.tar.gz
+# cd scons-2.3.0
+# python setup.py install
+
+# git clone "https://github.com/bear24rw/gamingcape_fceu.git"
+# cd gamingcape_fceu
+# scons
---
+++ software/sync.sh
@@ -0,0 +1,1 @@
+rsync -avz --stats --progress . root@192.168.1.105:
